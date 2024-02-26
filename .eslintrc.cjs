76da1718f264a1eb40d84bae0369dd66d8bb1b49
sooyeoniya <tndus1306403@naver.com>
2024-02-26 15:45:09 +0900

    First commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    '@typescript-eslint/eslint-plugin',
+    'prettier'
+  ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'prettier/prettier': [
+      'error', {
+        'endOfLine': 'auto'
+      }
+    ]
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="text-blue-400">
+            <h1 className="text-blue-500"> Hello hihihihi </h1>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+    );
+}
+
+export default App;
