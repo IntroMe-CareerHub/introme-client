52ef645cc6bb38e300d4c5c0a02ed57a9328b133
Hyun Seok Park <luckseok1@gmail.com>
2024-02-26 17:27:28 +0900

    [Test] Test Code

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
             <h1 className="text-blue-500 font-GmarketSansLight"> Hello hihihihi </h1>
             <h1 className="text-3xl font-bold font-GmarketSansBold">Hello world!</h1>
             <h1 className="text-3xl font-bold text-main-1 font-GmarketSansMedium">
-                test!!! 테스트코드
+                test!!! 테스트코드 테스트테스트
             </h1>
         </div>
     );
