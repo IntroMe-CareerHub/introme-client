c744e1c4d76dfa0548ee7ead96e4bb8b61614448
sooyeoniya <tndus1306403@naver.com>
2024-02-26 17:04:23 +0900

    [Style] TailwindCSS Setting

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import "./App.css";
 function App() {
     return (
         <div className="text-blue-400">
-            <h1 className="text-blue-500"> Hello hihihihi </h1>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-            <h1 className="text-3xl font-bold underline">test!!!</h1>
+            <h1 className="text-blue-500 font-GmarketSansLight"> Hello hihihihi </h1>
+            <h1 className="text-3xl font-bold font-GmarketSansBold">Hello world!</h1>
+            <h1 className="text-3xl font-bold text-main-1 font-GmarketSansMedium">
+                test!!! 테스트코드
+            </h1>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                "main-1": "#1DCD8B",
+                "main-2": "#4AD7A2",
+                "main-3": "#C6FFE9",
+                "my-blue": "#0085FF",
+                "my-green": "#0D9C18",
+                "my-purple": "#6A35FF",
+                "my-red": "#EA3323"
+            }
+        },
+        fontFamily: {
+            GmarketSansBold: ["GmarketSansBold"],
+            GmarketSansMedium: ["GmarketSansMedium"],
+            GmarketSansLight: ["GmarketSansLight"]
+        }
     },
     plugins: []
 };
