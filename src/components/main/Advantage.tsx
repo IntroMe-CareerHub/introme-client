7057601e773843ee56fb102e7eed47a1e9e870f7
Hyun Seok Park <luckseok1@gmail.com>
2024-03-02 19:25:55 +0900

    [Design] Advantage section UI

---
+++ src/components/main/Advantage.tsx
@@ -0,0 +1,13 @@
+import { MainProps } from "../../types/main";
+
+export default function Advantage({ title, sectionId, children }: MainProps) {
+    return (
+        <section
+            id={sectionId}
+            className="bg-main-2 flex flex-col items-center justify-center text-center h-full text-white space-y-5"
+        >
+            <h1 className="font-bold text-7xl">{title}</h1>
+            {children}
+        </section>
+    );
+}
