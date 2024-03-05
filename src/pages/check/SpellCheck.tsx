411b923a88851232d9b621581bcfad818c6ae50a
sooyeoniya <tndus1306403@naver.com>
2024-03-05 18:31:31 +0900

    [Design] Modify buttons and implement result details UI

--- src/pages/check/SpellCheck.tsx
+++ src/pages/check/SpellCheck.tsx
@@ -2,7 +2,7 @@ import { GrPowerReset } from "react-icons/gr";
 import { LuCopy } from "react-icons/lu";
 import { BsToggleOn } from "react-icons/bs";
 import { AiOutlineInfoCircle } from "react-icons/ai";
-import { FaCircle } from "react-icons/fa";
+import { FaArrowRight, FaCircle } from "react-icons/fa";
 
 export default function SpellCheck() {
     return (
@@ -45,24 +45,40 @@ export default function SpellCheck() {
                         </p>
                     </div>
                     <div className="flex w-full pt-4 gap-4 font-GmarketSansMedium text-sm">
-                        <div className="flex px-8 py-2 justify-center items-center bg-black bg-opacity-5 text-neutral-400 rounded-md shadow-btn">
+                        <button
+                            type="button"
+                            onClick={undefined}
+                            className="flex px-8 py-2 justify-center items-center bg-black bg-opacity-5 text-neutral-400 rounded-md shadow-btn"
+                        >
                             !@#$%
-                        </div>
-                        <div className="flex pl-6 pr-8 py-2 justify-center items-center bg-white rounded-md shadow-btn gap-1">
+                        </button>
+                        <button
+                            type="button"
+                            onClick={undefined}
+                            className="flex pl-6 pr-8 py-2 justify-center items-center bg-white rounded-md shadow-btn gap-1"
+                        >
                             <div className="text-neutral-400">
                                 <GrPowerReset />
                             </div>
                             초기화
-                        </div>
-                        <div className="flex px-8 py-2 justify-center items-center bg-black bg-opacity-5 text-neutral-400 rounded-md shadow-btn">
+                        </button>
+                        <button
+                            type="button"
+                            onClick={undefined}
+                            className="flex px-8 py-2 justify-center items-center bg-black bg-opacity-5 text-neutral-400 rounded-md shadow-btn"
+                        >
                             !@#$%
-                        </div>
-                        <div className="flex pl-4 pr-6 py-2 justify-center items-center bg-white rounded-md shadow-btn gap-1">
+                        </button>
+                        <button
+                            type="button"
+                            onClick={undefined}
+                            className="flex pl-4 pr-6 py-2 justify-center items-center bg-white rounded-md shadow-btn gap-1"
+                        >
                             <div className="text-neutral-400">
                                 <LuCopy />
                             </div>
                             전체 복사
-                        </div>
+                        </button>
                     </div>
                 </div>
                 <div className="flex flex-col w-full h-full py-4 pr-4 pl-2">
@@ -87,24 +103,73 @@ export default function SpellCheck() {
                             </div>
                         </div>
                     </div>
-                    <div className="w-full h-full bg-white rounded-xl shadow-main font-GmarketSansMedium p-4">
-                        <p className="w-full h-full text-sm grow">결과 내용</p>
+                    <div className="w-full h-full bg-white rounded-xl shadow-main font-GmarketSansMedium p-4 overflow-auto">
+                        <div className="flex flex-col w-full h-full text-sm gap-3">
+                            <div className="grid grid-cols-3">
+                                <p className="text-my-blue">!!!!!</p>
+                                <div className="flex gap-2 items-center">
+                                    <FaArrowRight />
+                                    <p>특수문자</p>
+                                </div>
+                                <div className="flex justify-end">
+                                    <button
+                                        type="button"
+                                        onClick={undefined}
+                                        className="bg-my-blue py-1.5 px-2.5 rounded-md text-xs text-white"
+                                    >
+                                        삭제
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-3">
+                                <p className="text-my-purple">안뇽</p>
+                                <div className="flex gap-2 items-center">
+                                    <FaArrowRight />
+                                    <p className="text-my-purple">안녕</p>
+                                </div>
+                                <div className="flex justify-end">
+                                    <button
+                                        type="button"
+                                        onClick={undefined}
+                                        className="bg-my-purple py-1.5 px-2.5 rounded-md text-xs text-white"
+                                    >
+                                        수정
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="flex w-full pt-4 justify-between font-GmarketSansMedium text-sm">
                         <div className="flex items-center pl-2 text-my-red">교정 개수 3개</div>
                         <div className="flex gap-4">
-                            <div className="flex px-8 py-2 justify-center items-center bg-black bg-opacity-5 text-neutral-400 rounded-md shadow-btn">
+                            <button
+                                type="button"
+                                onClick={undefined}
+                                className="flex px-8 py-2 justify-center items-center bg-black bg-opacity-5 text-neutral-400 rounded-md shadow-btn"
+                            >
                                 !@#$%
-                            </div>
-                            {/*<div className="flex px-7 py-2 justify-center items-center bg-white rounded-md shadow-btn">*/}
+                            </button>
+                            {/*<button*/}
+                            {/*    type="button"*/}
+                            {/*    onClick={undefined}*/}
+                            {/*    className="flex px-7 py-2 justify-center items-center bg-white rounded-md shadow-btn"*/}
+                            {/*>*/}
                             {/*    전체 수정*/}
-                            {/*</div>*/}
-                            {/*<div className="flex px-8 py-2 justify-center items-center bg-black bg-opacity-5 text-neutral-400 rounded-md shadow-btn">*/}
+                            {/*</button>*/}
+                            {/*<button*/}
+                            {/*    type="button"*/}
+                            {/*    onClick={undefined}*/}
+                            {/*    className="flex px-8 py-2 justify-center items-center bg-black bg-opacity-5 text-neutral-400 rounded-md shadow-btn"*/}
+                            {/*>*/}
                             {/*    !@#$%*/}
-                            {/*</div>*/}
-                            <div className="flex px-7 py-2 justify-center items-center bg-white rounded-md shadow-btn">
+                            {/*</button>*/}
+                            <button
+                                type="button"
+                                onClick={undefined}
+                                className="flex px-7 py-2 justify-center items-center bg-white rounded-md shadow-btn"
+                            >
                                 다시 검사
-                            </div>
+                            </button>
                         </div>
                     </div>
                 </div>
