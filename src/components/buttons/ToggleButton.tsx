ad5282a84b6c8576ae5ea2e6570fb0d3cf0231e7
Hyun Seok Park <luckseok1@gmail.com>
2024-03-16 07:22:24 +0900

    [Feat] 공백 & 글자수 기능 구현

--- src/components/buttons/ToggleButton.tsx
+++ src/components/buttons/ToggleButton.tsx
@@ -1,21 +1,26 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function ToggleButton() {
-    const [isOn, setisOn] = useState(false);
-    const toggleHandler = () => {
-        setisOn(!isOn);
-    };
+interface ToggleButtonProps {
+    includeSpaces?: boolean;
+    handleToggleSpaces?: () => void;
+}
+
+export default function ToggleButton({ includeSpaces, handleToggleSpaces }: ToggleButtonProps) {
+    // const [isOn, setisOn] = useState(false);
+    // const toggleHandler = () => {
+    //     setisOn(!isOn);
+    // };
 
     return (
         <div>
             <div
                 className={`relative cursor-pointer w-7 h-4 rounded-full
-                ${isOn ? "bg-main-1 transition-all duration-500" : "bg-gray-300"}`}
-                onClick={toggleHandler}
+                ${includeSpaces ? "bg-main-1 transition-all duration-500" : "bg-gray-300"}`}
+                onClick={handleToggleSpaces}
             >
                 <div
                     className={`absolute top-0.5 left-0.5 bg-white rounded-full transition-all duration-500 w-3 h-3
-                    ${isOn ? "left-3.5" : ""}`}
+                    ${includeSpaces ? "left-3.5" : ""}`}
                 />
             </div>
         </div>
