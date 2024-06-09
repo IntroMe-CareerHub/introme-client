df60ea5abd61a08cc8e1764f39118ae19516624a
philos1234 <ohhyun1948@gmail.com>
2024-06-09 21:40:13 +0900

    lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ module.exports = {
     ],
     rules: {
         "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+        "@typescript-eslint/no-explicit-any": "off",
         "no-unused-vars": "off",
         "@typescript-eslint/no-unused-vars": "off",
         "@typescript-eslint/ban-ts-comment": "off",
