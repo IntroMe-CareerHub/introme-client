9d41b0b9091402d2c5402ae06d433a3d3599cb93
philos1234 <ohhyun1948@gmail.com>
2024-06-29 19:43:23 +0900

    rebase develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import { Route, Routes } from "react-router-dom";
 import Router from "./components/Router.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 import MainFrame from "./layouts/MainFrame.tsx";
 import SignIn from "./pages/login/SignIn.tsx";
 import SignInSuccess from "./pages/login/SignInSuccess.tsx";
+
 import { UserProvider } from "./contexts/UserContext.tsx";
 
 export default function App() {
     return (
-        <UserProvider>
-            <Routes>
-                <Route path="/sign-in" element={<SignIn />} />
-                <Route path="/login-success" element={<SignInSuccess />} />
-                <Route
-                    path="/"
-                    element={
-                        <MainFrame>
-                            <Router />
-                        </MainFrame>
-                    }
-                />
-            </Routes>
-        </UserProvider>
+        <QueryClientProvider client={queryClient}>
+            <UserProvider>
+                <Routes>
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/login-success" element={<SignInSuccess />} />
+                    <Route
+                        path="/"
+                        element={
+                            <MainFrame>
+                                <Router />
+                            </MainFrame>
+                        }
+                    />
+                </Routes>
+                <ReactQueryDevtools />
+            </UserProvider>
+        </QueryClientProvider>
     );
 }
