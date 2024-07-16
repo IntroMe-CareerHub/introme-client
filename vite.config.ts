2fae7e26713001c1c7101b968ebc4ad5e1a9e678
Hyun Seok Park <luckseok1@gmail.com>
2024-07-16 20:01:55 +0900

    [Fix] vite config 설정

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
             "/api": {
                 target: apiUrl,
                 changeOrigin: true,
+                secure: false,
                 rewrite: path => path.replace(/^\/api/, "/api")
             },
             "/logout": apiUrl
